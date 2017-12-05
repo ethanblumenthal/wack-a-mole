f46f1f2109271d38022648c3efeaf8c1c6b9e1a2
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-05 14:17:15 -0800

    added score functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var board = document.querySelector('#board');
 var score = document.querySelector('#score');
+var counter = 0;
 
 function createGrid() {
     for (var i = 0; i < 9; i++) {
@@ -17,26 +18,25 @@ function randomInt(num){
 }
 
 function createMole(){
-    holes[randomInt(9)].innerHTML = '<img src="css/mole.png">';
+    holes[randomInt(9)].style.background = 'black';
 }
 
 function deleteMole() {
     holes.forEach(function(e) {
-        e.innerHTML = '';
+        e.style.background = 'brown';
     });
 }
 
 start.addEventListener('click', function() {    
-    setInterval(createMole, 1000);
-    setInterval(deleteMole, 999);
+    setInterval(createMole, 500);
+    setInterval(deleteMole, 499);
 });
 
-holes.forEach(function(e) {
-    var counter = 0;    
+holes.forEach(function(e) {    
     e.addEventListener('click', function() {
-        if (e.innerHTML === '<img src="css/mole.png">') {
+        if (this.style.background === 'black') {
             counter++;
         }
-        score.innerHTML = 'SCORE: ' + counter;
+        score.innerText = 'SCORE: ' + counter;
     });
 });
