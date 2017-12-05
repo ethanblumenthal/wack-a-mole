db092844f03bba6aaaea4bf554962f20aa176021
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-05 11:38:36 -0800

    added initial js logic

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
 var board = document.querySelector('#board');
 var holes = document.querySelectorAll('.hole');
+var start = document.querySelector('#start');
+var score = document.querySelector('#score');
 
 function createGrid() {
     for (var i = 0; i < 9; i++) {
         var hole = document.createElement('div');
         hole.className = 'hole';
-        hole.id = i;       
         board.appendChild(hole);
     }
 };
 createGrid();
 
-// function randomHole() {
-        
-// };
+function randomMole() {
+    var mole = holes[Math.floor(Math.random() * holes.length)];    
+    mole.style.background = 'black';
+};
 
-// function createMole() {
-    
-// };
-// interval = setInterval(createMole, 1000);
+start.addEventListener('click', function() {    
+    interval = setInterval(randomMole, 1000);
+});
 
 function updateScore() {
-    let score = 0;
-    document.getElementById('score').innerHTML = 'Score: ' + score;
-    mole.onclick = function() {
-        score += 1;
-        document.getElementById('score').innerHTML = 'Score: ' + score;
-    }
+    var counter = 0;
+    moles.addEventListener('click', function() {
+        counter++;
+        score.innerText = 'SCORE: ' + counter;
+    });
 };
-updateScore();
